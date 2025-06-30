d50dd95f52594bd1557b3e9bfe7e50de47db2c60
xyugautomation <shiv.xyug@gmail.com>
2025-06-30 11:27:48 +0530

    Just first program i push ..!

---
+++ String_Programs1/src/main/java/Simple_Programs/String_Litteter_pro.java
@@ -0,0 +1,21 @@
+package Simple_Programs;
+
+public class String_Litteter_pro {
+
+	public static void main(String[] args) {
+		
+		String name ="shivshankar";
+		
+		System.out.println(name);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
